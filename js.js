eb2282b9a2e1f8d185f879d2565675c145b81576
Admilson Landim <ady.pina10@gmail.com>
2022-09-25 18:54:22 -0100

    basics

---
+++ js.js
@@ -0,0 +1,28 @@
+
+// Iniciando Pratica: You Dont Know JS /*  */
+
+a = "21";
+b  = a + 3;
+c = Number(a);
+
+console.log(b);
+
+/* alert(b);
+
+var  age = prompt("Please tell your age: ");  // solicitar um entrada na tela
+
+console.log(age);
+
+var ten_years = parseInt(age) + 10;
+
+console.log("In ten years you've ", ten_years, " years old !!!");
+
+console.log("In ten years you've ", Number(age) + 9, " years old !!!");  // Converter age em numero
+
+*/
+
+
+console.log(a);
+console.log(c);
+
+
