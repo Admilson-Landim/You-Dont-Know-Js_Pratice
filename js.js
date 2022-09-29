11aed8df6372499ce8bcdfe636d7b4be73e37a5a
Admilson Landim <ady.pina10@gmail.com>
2022-09-29 16:54:36 -0100

    Update js.js
    parrei em funcao

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 
 // Iniciando Pratica: You Dont Know JS /*  */
 
+/* 
 a = "21";
 b  = a + 3;
 c = Number(a);
@@ -19,10 +20,38 @@ console.log("In ten years you've ", ten_years, " years old !!!");
 
 console.log("In ten years you've ", Number(age) + 9, " years old !!!");  // Converter age em numero
 
-*/
-
 
 console.log(a);
 console.log(c);
+
+
+const TAX_RATE = 2; 
+const ACCESSORY_PRICE = 9.99;
+
+var amount = 99.99;
+var bank_balance = 302.13;
+
+if(amount < bank_balance) {
+    console.log("Vou levar este celular...");
+    amount = amount + ACCESSORY_PRICE;
+    console.log(amount);
+
+}else {
+    console.log("Não, obrigado !!!"); 
+}
+
+*/
+
+var i = 0;
+
+while (true){
+    if( (i <= 9) === false) { 
+        break;
+    }
+
+    console.log(i);
+    i += 1;
+}
+
 
 
