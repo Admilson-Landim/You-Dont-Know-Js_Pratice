b44359ecffd2383cd59468e9b2dcf6e879326edb
Admilson Landim <ady.pina10@gmail.com>
2022-09-30 21:20:54 -0100

    Update js.js
    end iniciando JS

--- js.js
+++ js.js
@@ -1,57 +1,81 @@
 
 // Iniciando Pratica: You Dont Know JS /*  */
-
-/* 
-a = "21";
-b  = a + 3;
-c = Number(a);
-
-console.log(b);
-
-/* alert(b);
-
-var  age = prompt("Please tell your age: ");  // solicitar um entrada na tela
-
-console.log(age);
-
-var ten_years = parseInt(age) + 10;
-
-console.log("In ten years you've ", ten_years, " years old !!!");
-
-console.log("In ten years you've ", Number(age) + 9, " years old !!!");  // Converter age em numero
-
-
-console.log(a);
-console.log(c);
-
-
-const TAX_RATE = 2; 
-const ACCESSORY_PRICE = 9.99;
-
 var amount = 99.99;
-var bank_balance = 302.13;
 
-if(amount < bank_balance) {
-    console.log("Vou levar este celular...");
-    amount = amount + ACCESSORY_PRICE;
-    console.log(amount);
 
-}else {
-    console.log("Não, obrigado !!!"); 
-}
 
-*/
+function antesFuncção() {
+    /* 
+    a = "21";
+    b  = a + 3;
+    c = Number(a);
 
-var i = 0;
+    console.log(b);
 
-while (true){
-    if( (i <= 9) === false) { 
-        break;
+    /* alert(b);
+
+    var  age = prompt("Please tell your age: ");  // solicitar um entrada na tela
+
+    console.log(age);
+
+    var ten_years = parseInt(age) + 10;
+
+    console.log("In ten years you've ", ten_years, " years old !!!");
+
+    console.log("In ten years you've ", Number(age) + 9, " years old !!!");  // Converter age em numero
+
+
+    console.log(a);
+    console.log(c);
+
+
+    const TAX_RATE = 2; 
+    const ACCESSORY_PRICE = 9.99;
+
+    var amount = 99.99;
+    var bank_balance = 302.13;
+
+    if(amount < bank_balance) {
+        console.log("Vou levar este celular...");
+        amount = amount + ACCESSORY_PRICE;
+        console.log(amount);
+
+    }else {
+        console.log("Não, obrigado !!!"); 
     }
 
-    console.log(i);
-    i += 1;
+    */
 }
+
+
+function testeWhile(){
+    var i = 0;
+
+    while (true){
+        if( (i <= 9) === false) { 
+            break;
+        }
+
+        console.log(i);
+        i += 1;
+    }
+
+}
+
+
+function printAmount(amt){
+    console.log(amt.toFixed(2));
+}
+
+function formatAmount(){
+    return "$" + amount.toFixed(2);
+}
+
+printAmount(amount * 2);
+
+amount = formatAmount();
+
+console.log(amount);
 
 
 
